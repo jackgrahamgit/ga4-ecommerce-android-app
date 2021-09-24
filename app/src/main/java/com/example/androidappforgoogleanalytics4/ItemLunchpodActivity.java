005a1c0816f307d51431c72b106f2fe8f53ff31a
Jack Graham <jackgraham@google.com>
2021-09-24 13:40:59 -0700

    brand new version

---
+++ app/src/main/java/com/example/androidappforgoogleanalytics4/ItemLunchpodActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidappforgoogleanalytics4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ItemLunchpodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_lunchpod);
+    }
+}
