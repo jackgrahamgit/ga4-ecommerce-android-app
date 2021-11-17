357e29455c2f0774a0c3671e36944ed38a4a502a
aoppenheimer <aoppenheimer@google.com>
2021-11-17 11:00:31 -0800

    Added remaining unchecked files

---
+++ app/src/test/java/com/exemplary/androidappforgoogleanalytics4/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.exemplary.androidappforgoogleanalytics4;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * exemplary local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class exemplaryUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
